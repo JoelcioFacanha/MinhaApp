2ecf96673f614ceeb7da5f60751a302ec5c4dddf
Joelcio Façanha <joelciomoreira@gmail.com>
2021-09-23 14:39:56 -0300

    Working migration of identity

--- src/DevIO.App/Data/Migrations/20210923173711_Initial_Identity.Designer.cs
+++ src/DevIO.App/Data/Migrations/20210923173711_Initial_Identity.Designer.cs
@@ -4,14 +4,16 @@ using DevIO.App.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DevIO.App.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210923173711_Initial_Identity")]
+    partial class Initial_Identity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
